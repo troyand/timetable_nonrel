d5358cda263b93eb4768a3e585a82afae2c7f2cb
troyand <troyanovsky@gmail.com>
2013-10-06 23:24:09 +0300

    handle None in similar strings

--- timetable/university/utils.py
+++ timetable/university/utils.py
@@ -8,7 +8,7 @@ def get_potential_duplicates(strings):
     dups = set()
     for string_a, string_b in itertools.combinations(strings, 2):
         jaro_distance =  jellyfish.jaro_distance(
-                unidecode(string_a), unidecode(string_b))
+                unidecode(string_a or ''), unidecode(string_b or ''))
         if 0.9 < jaro_distance < 1:
             print jaro_distance, string_a, string_b
             dups.add(string_a)
