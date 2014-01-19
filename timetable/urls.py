22d4b71957cf165985ff69b72a34777c72133e99
troyand <troyanovsky@gmail.com>
2014-01-19 13:41:20 -0800

    retina favicon fix

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,6 +8,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'timetable.university.views.home', name='home'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='static/icons/favicon.ico')),
     url(r'^info/$', 'timetable.university.views.info'),
     url(r'^accounts/login/$', 'django_usic_sso.views.login', name='login'),
 #    url(r'^accounts/login/$', 'timetable.university.auth_debug.login', name='login'),
